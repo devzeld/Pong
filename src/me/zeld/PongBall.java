d890c5eed4550f5c72963f72204aa0362b649c7d
devzeld <zeld.exe.dev@gmail.com>
2023-11-16 22:41:26 +0100

    finished the rackets!:)

--- src/me/zeld/PongBall.java
+++ src/me/zeld/PongBall.java
@@ -2,18 +2,15 @@ package me.zeld;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.concurrent.TimeUnit;
 
 public class PongBall extends JPanel {
     public int XPOS;
     public int YPOS;
-    private final Board B;
     public final Color COLOR;
     PongBall(Board b, int xPos, int yPos, int size) {
         XPOS = xPos;
         YPOS = yPos;
         COLOR = new Color(0x000020);
-        B = b;
 
         setBackground(COLOR);
         setBounds(XPOS,YPOS, size, size);
